2783af68b4b20682d915200e08639f12202d845f
MehreenAslam23 <mehru.aslam515@gmail.com>
2024-03-19 13:56:41 +0500

    Designed Footer

--- src/Components/heroSection.js
+++ src/Components/heroSection.js
@@ -1,6 +1,7 @@
 import "../Components/heroSection.css"
 import image2 from '../Images/image2.png'
 import image3 from '../Images/image3.svg'
+import image5 from '../Images/image5.svg'
 function heroSection()
 {
     return(
@@ -9,9 +10,35 @@ function heroSection()
             <h1>GOOD<span>-</span>FOOD</h1>
             <img className="img2" src={image2}></img>
             <img  className="img4" src={image3}></img>
-            <h1>GOOD MOOD</h1>
-            {/* <h1>FOOD</h1> */}
+            <h1>MOOD</h1>
+            <div className="slider">
+                <ul className="list">
+                    <li>PACKED WITH FLAVOUR</li>
+                    <img src={image5}></img>
+                    <li>100% PLANT BASED</li>
+                    <img src={image5}></img>
+                    <li>DAIRY FREE</li>
+                    <img src={image5}></img>
+                </ul>
+                <ul className="list">
+                    <li>PACKED WITH FLAVOUR</li>
+                    <img src={image5}></img>
+                    <li>100% PLANT BASED</li>
+                    <img src={image5}></img>
+                    <li>DAIRY FREE</li>
+                    <img src={image5}></img>
+                </ul>
+                <ul className="list">
+                    <li>PACKED WITH FLAVOUR</li>
+                    <img src={image5}></img>
+                    <li>100% PLANT BASED</li>
+                    <img src={image5}></img>
+                    <li>DAIRY FREE</li>
+                    <img src={image5}></img>
+                </ul>
+            </div>
         </div>
+
 
     )
 }
